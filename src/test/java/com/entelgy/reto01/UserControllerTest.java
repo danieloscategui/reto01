39ce3754839077ff6e5c06a0813326ee30a622c5
Daniel Oscategui <danieloscategui@gmail.com>
2022-05-31 15:40:24 -0500

    test controller layer

---
+++ src/test/java/com/entelgy/reto01/UserControllerTest.java
@@ -0,0 +1,30 @@
+package com.entelgy.reto01;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testPostUserIsOk() throws Exception {
+        mockMvc.perform(post("/reto")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testPostUserExistData() throws Exception {
+        mockMvc.perform(post("/reto")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data[*].id").isNotEmpty());
+    }
+}
