4ca9a995aae342125e88ef7813c8472990f2b4b2
Daniel Oscategui <danieloscategui@gmail.com>
2022-06-01 12:12:11 -0500

    update formatter and use ZoneDateTime

--- src/main/java/com/entelgy/reto01/util/DateUtil.java
+++ src/main/java/com/entelgy/reto01/util/DateUtil.java
@@ -3,13 +3,15 @@ package com.entelgy.reto01.util;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
     public static String getISO8601DateTimeFormatted(LocalDateTime localDateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        return formatter.format(localDateTime);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
+        return ZonedDateTime.now().format(formatter);
     }
+    
     public static String getCurrentDateFormatted() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return formatter.format(LocalDate.now());
