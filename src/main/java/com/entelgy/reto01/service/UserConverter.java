619c18766f57dfd4fd6cdd68164ac2b0fc52db68
Daniel Oscategui <danieloscategui@gmail.com>
2022-06-02 08:47:47 -0500

    use class UserConverter

--- src/main/java/com/entelgy/reto01/service/UserConverter.java
+++ src/main/java/com/entelgy/reto01/service/UserConverter.java
@@ -1,11 +1,12 @@
 package com.entelgy.reto01.service;
 
 import com.entelgy.reto01.external.model.User;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
+@Component
 public class UserConverter {
     public List<String> convert(List<User> users) {
         return users.stream().map(
--- src/main/java/com/entelgy/reto01/service/UserService.java
+++ src/main/java/com/entelgy/reto01/service/UserService.java
@@ -3,24 +3,21 @@ package com.entelgy.reto01.service;
 import com.entelgy.reto01.api.response.UserResponse;
 import com.entelgy.reto01.external.ApiClient;
 import com.entelgy.reto01.external.RestException;
-import com.entelgy.reto01.external.model.User;
 import com.entelgy.reto01.external.model.UserApiResponse;
 import com.entelgy.reto01.util.DateUtil;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class UserService {
 
     private final ApiClient apiClient;
     private final UserConverter userConverter;
 
-    public UserService(ApiClient apiClient) {
+    public UserService(ApiClient apiClient, UserConverter userConverter) {
         this.apiClient = apiClient;
-        this.userConverter = new UserConverter();
+        this.userConverter = userConverter;
     }
 
     public UserResponse processData() throws UserServiceException {
@@ -28,7 +25,6 @@ public class UserService {
             UserApiResponse userApiResponse;
             userApiResponse = apiClient.getExternalUsers();
 
-//            Set<String> data = convert(userApiResponse.getData());
             List<String> data = userConverter.convert(userApiResponse.getData());
             UserResponse userResponse = new UserResponse();
             userResponse.setOperationDate(DateUtil.getISO8601DateTimeFormatted(LocalDateTime.now()));
@@ -37,13 +33,5 @@ public class UserService {
         } catch (RestException e) {
             throw new UserServiceException(e.getMessage(), e);
         }
-    }
-
-    private Set<String> convert(List<User> userApiResponse) {
-        return userApiResponse.stream().map(this::joinData).collect(Collectors.toSet());
-    }
-
-    private String joinData(User user) {
-        return String.join("|", String.valueOf(user.getId()), user.getLast_name(), user.getEmail());
     }
 }
